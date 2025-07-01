544fa69978c12efaa7ca0b37e36a2a3be6e7a595
Zhoulei <z5436599@ad.unsw.edu.au>
2025-07-01 13:00:03 +1000

    Replace main branch content with other-branch-name

--- research_assistant/settings.py
+++ research_assistant/settings.py
@@ -23,3 +23,15 @@ CACHE_TYPE = (
     "flask_caching.backends.SimpleCache"  # Can be "MemcachedCache", "RedisCache", etc.
 )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+FLASK_ENV  = env.str("FLASK_ENV", "production")
+DATABASE_URL = env.str("DATABASE_URL")
+
+MAIL_SERVER   = env.str("MAIL_SERVER")
+MAIL_PORT     = env.int("MAIL_PORT")
+MAIL_USE_TLS  = env.bool("MAIL_USE_TLS", False)
+MAIL_USERNAME = env.str("MAIL_USERNAME")
+MAIL_PASSWORD = env.str("MAIL_PASSWORD")
+MAIL_DEFAULT_SENDER = env.str("MAIL_USERNAME")
+WTF_CSRF_ENABLED = False
+SEND_FILE_MAX_AGE_DEFAULT = env.int("SEND_FILE_MAX_AGE_DEFAULT", 0)
+
