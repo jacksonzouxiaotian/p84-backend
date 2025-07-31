4055b0f547d2e4a068c382b86899941a6c7de9f1
贺建军 <hejianjun@192.168.20.62>
2025-07-31 14:47:10 +1000

    plan

--- research_assistant/citation_styles.py
+++
@@ -1,9 +0,0 @@
-def generate_citation(ref, style="APA"):
-    if style == "APA":
-        return f"{ref.authors} ({ref.year}). {ref.title}. {ref.source}."
-    elif style == "MLA":
-        return f"{ref.authors}. \"{ref.title}.\" {ref.source}, {ref.year}."
-    elif style == "Chicago":
-        return f"{ref.authors}. {ref.title}. {ref.source}, {ref.year}."
-    else:
-        return f"{ref.authors} ({ref.year}). {ref.title}."
